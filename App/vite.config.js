a301fdcbdc602edca7d6872a097644c1ba6cfaef
EJCP3 <euddycabrera03@gmail.com>
2026-05-02 21:05:31 -0400

    v3

--- App/vite.config.js
+++ App/vite.config.js
@@ -6,4 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss(),],
+  server: {
+    proxy: {
+      '/api-proxy': {
+        target: 'http://myinventario.runasp.net/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-proxy/, ''),
+      },
+    },
+  },
 })
